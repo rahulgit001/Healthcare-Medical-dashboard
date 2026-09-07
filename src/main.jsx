142a8f0f92dcf8bd3364a4edbea1a7e16b1b7be3
Rahul kumar <131306879+rahulgit001@users.noreply.github.com>
2026-09-07 13:34:51 +0000

    Fix GitHub Pages deployment: switch to HashRouter for proper routing on subdirectory

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App.jsx";
 import { ThemeProvider } from "./context/ThemeContext.jsx";
 import { DashboardProvider } from "./context/DashboardContext.jsx";
@@ -10,9 +10,9 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider>
       <DashboardProvider>
-        <BrowserRouter>
+        <HashRouter>
           <App />
-        </BrowserRouter>
+        </HashRouter>
       </DashboardProvider>
     </ThemeProvider>
   </React.StrictMode>
